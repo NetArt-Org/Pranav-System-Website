bc2824f440540d3ff3261a151e9595013d777d43
@Kavita2599 <kavita99@netart.io>
2025-02-21 17:17:12 +0530

    package.json updated

--- plasmic-init.js
+++ plasmic-init.js
@@ -1,4 +1,5 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+import contactForm from "./components/contactForm";
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -23,3 +24,8 @@ export const PLASMIC = initPlasmicLoader({
 // https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
 
 // PLASMIC.registerComponent(...);
+PLASMIC.registerComponent(contactForm, {
+  name: 'contactForm',
+  props: {
+  },
+});
